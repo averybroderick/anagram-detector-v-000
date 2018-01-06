79542c3e29916e23fcd6ee54a7552bba0f7fbfdd
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-06 17:08:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word_to_match
+
+  def initialize(word)
+    @word_to_match = word
+  end
+
+  def match(arr_of_words)
+    result = []
+    arr_of_words.collect do |word|
+      word.split("").sort.detect do |split_sorted_word|
+        if @word_to_match.split("").sort == split_sorted_word
+          result << word
+        else
+          nil
+        end
+      end
+    end
+  result
+  end
+
+end

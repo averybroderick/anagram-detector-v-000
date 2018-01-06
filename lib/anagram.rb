fce56115644aff00259d0f241eb007e37f99c294
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-06 17:11:05 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,5 +1,4 @@
 # Your code goes here!
-require 'pry'
 class Anagram
   attr_accessor :word_to_match
 
@@ -12,7 +11,6 @@ class Anagram
     arr_of_words.collect do |word|
       word.split("").sort.detect do |split_sorted_word|
         if @word_to_match.split("").sort == split_sorted_word
-          binding.pry
           result << word
         else
           nil
